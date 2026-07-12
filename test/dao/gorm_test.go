28a5cc38221f2a648a330704bb91f97145374da4
xy817-maker <xy817-maker@users.noreply.github.com>
2026-07-12 22:04:50 +0800

    fix: 修复测试文件字段名与模型不匹配的问题

--- test/dao/gorm_test.go
+++ test/dao/gorm_test.go
@@ -12,12 +12,12 @@ import (
 func TestCreate(t *testing.T) {
 	userInfo := &model.UserInfo{
 		Uuid:      "U" + strconv.Itoa(random.GetRandomInt(11)),
-		NickName:  "apylee",
-		TelePhone: "180323532112",
+		Nickname:  "apylee",
+		Telephone: "180323532112",
 		Email:     "1212312312@qq.com",
 		Password:  "123456",
 		CreatedAt: time.Now(),
-		IsAdmin:   true,
+		IsAdmin:   1,
 	}
 	_ = dao.GormDB.Create(userInfo)
 }
